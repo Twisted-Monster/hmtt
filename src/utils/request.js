d39c67901bd1501d52cf81fc079ed66d1357a577
Tian <1767082982@qq.com>
2022-06-26 14:10:33 +0800

    master

---
+++ src/utils/request.js
@@ -0,0 +1,17 @@
+// 基于 axios 封装的请求模块
+import ajax from 'axios'
+// 新建一个新的axios实例
+const axios = ajax.create({
+  baseURL: 'http://123.57.109.30:8000' // 基地址
+})
+
+// 导出自定义函数, 参数对象解构赋值
+export default ({ url, method = 'GET', params, data, headers }) => {
+  return axios({
+    url: url,
+    method: method,
+    params: params,
+    data: data,
+    headers: headers
+  })
+}
